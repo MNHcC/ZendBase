ed853f32dbf027bbecebecde29e503b14c485808
carschrotter <carschrotter@carschrotter-work>
2016-08-05 15:41:14 +0200

    namespace and composer updates

--- src/Module/BasicModuleInterface.php
+++ src/Module/BasicModuleInterface.php
@@ -1,16 +1,19 @@
 <?php
-namespace MNHcC\Module;
 
-use Zend\ModuleManager\Feature\BootstrapListenerInterface;
-use Zend\EventManager\EventManagerAwareInterface;
-use ServiceLocatorAwareInterface
+namespace MNHcC\Module {
 
-/**
- * Description of BasicModuleInterface
- *
- * @author carschrotter
- */
-interface BasicModuleInterface extends BootstrapListenerInterface, EventManagerAwareInterface, ServiceLocatorAwareInterface{
+    use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+    use Zend\EventManager\EventManagerAwareInterface;
 
-    
+    use ServiceLocatorAwareInterface;
+
+    /**
+     * Description of BasicModuleInterface
+     *
+     * @author carschrotter
+     */
+    interface BasicModuleInterface extends BootstrapListenerInterface, EventManagerAwareInterface, ServiceLocatorAwareInterface {
+        
+    }
+
 }
